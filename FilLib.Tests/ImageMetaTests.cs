baf49b25e36b571a6ba6cfda2138426f5fd4423e
SnakE <snake.scaly@gmail.com>
2021-12-05 08:37:11 +0300

    Image "tail" metadata parser

---
+++ FilLib.Tests/ImageMetaTests.cs
@@ -0,0 +1,188 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace FilLib.Tests
+{
+    public class ImageMetaTests
+    {
+        private readonly byte[] _validMeta =
+        {
+            0x00, 0x00, 0x00, 0x00, 0xD6, 0xD2, 0x6D, 0x30, 0x0F, 0x8A, 0xC0, 0x0E, 0xD6, 0xF3, 0x7A, 0x0B,
+            0xE1, 0xE7, 0xE1, 0xF4, 0xB7, 0xA0, 0xA0, 0xA0, 0x0F, 0x0F, 0x4C, 0x0E, 0x84, 0x73, 0x7F, 0x8B,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0F, 0x0E, 0xC5, 0xA7, 0x50, 0x73, 0x76, 0xFB,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0xC3, 0x4F, 0x4D, 0x4D, 0x45, 0x4E, 0x54, 0xAE, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0,
+            0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0,
+            0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0,
+            0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0,
+            0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0,
+            0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0,
+            0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0,
+            0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0,
+            0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0,
+            0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0,
+            0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0,
+            0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0, 0xA0,
+        };
+
+        private readonly uint[] _expectedColors =
+        {
+            0xFF000000,
+            0xFFFFFFFF,
+            0xFF880000,
+            0xFFAAFFEE,
+            0xFFCC44CC,
+            0xFF00CC55,
+            0xFF0000AA,
+            0xFFEEEE77,
+            0xFFDD8855,
+            0xFF664400,
+            0xFFFF7777,
+            0xFF333333,
+            0xFF777777,
+            0xFFAAFF66,
+            0xFF0088FF,
+            0xFFBBBBBB,
+        };
+
+        [Fact]
+        public void TryParse_AcceptsValidData()
+        {
+            var fil = new Fil { Type = new FilType(0x84), Sectors = _validMeta };
+
+            var success = ImageMeta.TryParse(fil, out var meta);
+
+            Assert.True(success);
+            Assert.NotNull(meta);
+            Assert.Equal(ImageMeta.Mode.Agat_256_256_Pal4, meta.DisplayMode);
+            Assert.True(meta.IsGigaScreen);
+            Assert.Equal(ImageMeta.Palette.Agat_4, meta.PaletteType);
+            Assert.Equal(_expectedColors, meta.CustomPalette);
+            Assert.Equal("АГАТ7", meta.Charset);
+            Assert.Equal("Comment.", meta.Comment);
+        }
+
+        [Theory]
+        [InlineData(0x10, ImageMeta.Mode.Agat_256_256_Mono)]
+        [InlineData(0x40, ImageMeta.Mode.Agat_64_64_Pal16)]
+        [InlineData(0x50, ImageMeta.Mode.Agat_128_128_Pal16)]
+        [InlineData(0x60, ImageMeta.Mode.Agat_256_256_Pal4)]
+        [InlineData(0x70, ImageMeta.Mode.Agat_512_256_Mono)]
+        [InlineData(0x80, ImageMeta.Mode.Agat_128_256_Pal16)]
+        [InlineData(0x90, ImageMeta.Mode.Agat_280_192_AppleHiRes)]
+        [InlineData(0x21, ImageMeta.Mode.Agat_T32_Pal16)]
+        [InlineData(0x31, ImageMeta.Mode.Agat_T64_Mono)]
+        [InlineData(0x41, ImageMeta.Mode.Agat_T64_Pal16)]
+        [InlineData(0xA1, ImageMeta.Mode.Agat_T32_Pal16_FgBg)]
+        [InlineData(0xC1, ImageMeta.Mode.Agat_T64_Pal16_FgBg)]
+        [InlineData(0x9A, ImageMeta.Mode.Apple_280_192_HiRes)]
+        [InlineData(0xAA, ImageMeta.Mode.Apple_T40)]
+        [InlineData(0xBA, ImageMeta.Mode.Apple_T80)]
+        [InlineData(0xCA, ImageMeta.Mode.Apple_40_48_LoRes)]
+        [InlineData(0xDA, ImageMeta.Mode.Apple_80_48_DoubleLoRes)]
+        [InlineData(0xEA, ImageMeta.Mode.Apple_140_192_DoubleHiResColor)]
+        [InlineData(0xFA, ImageMeta.Mode.Apple_560_192_DoubleHiResMono)]
+        [InlineData(0x1D, ImageMeta.Mode.Agat_256_256_Mono)]
+        [InlineData(0x4D, ImageMeta.Mode.Agat_64_64_Pal16)]
+        [InlineData(0x5D, ImageMeta.Mode.Agat_128_128_Pal16)]
+        [InlineData(0x6D, ImageMeta.Mode.Agat_256_256_Pal4)]
+        [InlineData(0x7D, ImageMeta.Mode.Agat_512_256_Mono)]
+        [InlineData(0x8D, ImageMeta.Mode.Agat_128_256_Pal16)]
+        [InlineData(0x9D, ImageMeta.Mode.Agat_280_192_AppleHiRes)]
+        [InlineData(0xA0, ImageMeta.Mode.Unknown)]
+        [InlineData(0x23, ImageMeta.Mode.Unknown)]
+        public void TryParse_AllValidDisplayModes(byte code, ImageMeta.Mode mode)
+        {
+            _validMeta[6] = code;
+            var fil = new Fil { Type = new FilType(0x84), Sectors = _validMeta };
+
+            var success = ImageMeta.TryParse(fil, out var meta);
+
+            Assert.True(success);
+            Assert.NotNull(meta);
+            Assert.Equal(mode, meta.DisplayMode);
+        }
+
+        // test all palette variants
+        [Theory]
+        [InlineData(0x00, ImageMeta.Palette.Agat_1)]
+        [InlineData(0x10, ImageMeta.Palette.Agat_2)]
+        [InlineData(0x20, ImageMeta.Palette.Agat_3)]
+        [InlineData(0x30, ImageMeta.Palette.Agat_4)]
+        [InlineData(0x80, ImageMeta.Palette.Agat_1_Gray)]
+        [InlineData(0x90, ImageMeta.Palette.Agat_2_Gray)]
+        [InlineData(0xA0, ImageMeta.Palette.Agat_3_Gray)]
+        [InlineData(0xB0, ImageMeta.Palette.Agat_4_Gray)]
+        [InlineData(0x40, ImageMeta.Palette.Unknown)]
+        [InlineData(0x11, ImageMeta.Palette.Agat_2)]
+        [InlineData(0xF0, ImageMeta.Palette.Custom)]
+        public void TryParse_AllValidPaletteTypes(byte code, ImageMeta.Palette palette)
+        {
+            _validMeta[7] = code;
+            var fil = new Fil { Type = new FilType(0x84), Sectors = _validMeta };
+
+            var success = ImageMeta.TryParse(fil, out var meta);
+
+            Assert.True(success);
+            Assert.NotNull(meta);
+            Assert.Equal(palette, meta.PaletteType);
+        }
+
+        [Fact]
+        public void TryParse_RejectsNonBFiles()
+        {
+            var fil = new Fil { Sectors = _validMeta };
+
+            var success = ImageMeta.TryParse(fil, out var meta);
+
+            Assert.False(success);
+            Assert.Null(meta);
+        }
+
+        [Fact]
+        public void TryParse_RejectsEmptySectors()
+        {
+            var fil = new Fil { Type = new FilType(4) };
+
+            var success = ImageMeta.TryParse(fil, out var meta);
+
+            Assert.False(success);
+            Assert.Null(meta);
+        }
+
+        [Fact]
+        public void TryParse_RejectsFilesWithIncompleteLastSector()
+        {
+            var fil = new Fil { Type = new FilType(4), Sectors = _validMeta.Take(_validMeta.Length - 1).ToArray() };
+
+            var success = ImageMeta.TryParse(fil, out var meta);
+
+            Assert.False(success);
+            Assert.Null(meta);
+        }
+
+        [Fact]
+        public void TryParse_RejectsFilesWithDataSizeNotDivisibleBy256()
+        {
+            var fil = new Fil { Type = new FilType(0x84) };
+            fil.SetData(new byte[257]);
+
+            var success = ImageMeta.TryParse(fil, out var meta);
+
+            Assert.False(success);
+            Assert.Null(meta);
+        }
+
+        [Fact]
+        public void TryParse_RejectsBadSignature()
+        {
+            _validMeta[4] = 42;
+            var fil = new Fil { Type = new FilType(0x84), Sectors = _validMeta };
+
+            var success = ImageMeta.TryParse(fil, out var meta);
+
+            Assert.False(success);
+            Assert.Null(meta);
+        }
+    }
+}
---
+++ FilLib/ImageMeta.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FilLib
+{
+    public class ImageMeta
+    {
+        public enum Mode
+        {
+            Unknown,
+            Agat_256_256_Mono,
+            Agat_64_64_Pal16,
+            Agat_128_128_Pal16,
+            Agat_256_256_Pal4,
+            Agat_512_256_Mono,
+            Agat_128_256_Pal16,
+            Agat_280_192_AppleHiRes,
+            Agat_T32_Pal16,
+            Agat_T64_Mono,
+            Agat_T64_Pal16,
+            Agat_T32_Pal16_FgBg,
+            Agat_T64_Pal16_FgBg,
+            Apple_280_192_HiRes,
+            Apple_T40,
+            Apple_T80,
+            Apple_40_48_LoRes,
+            Apple_80_48_DoubleLoRes,
+            Apple_140_192_DoubleHiResColor,
+            Apple_560_192_DoubleHiResMono,
+        }
+
+        public enum Palette
+        {
+            Unknown,
+            Agat_1,
+            Agat_2,
+            Agat_3,
+            Agat_4,
+            Agat_1_Gray,
+            Agat_2_Gray,
+            Agat_3_Gray,
+            Agat_4_Gray,
+            Custom,
+        }
+
+        public Mode DisplayMode { get; set; }
+
+        public bool IsGigaScreen { get; set; }
+
+        public Palette PaletteType { get; set; }
+
+        public IList<uint> CustomPalette { get; set; }
+
+        public string Charset { get; set; }
+
+        public string Comment { get; set; }
+
+        public static bool TryParse(Fil fil, out ImageMeta result)
+        {
+            result = null;
+
+            if (!fil.Type.HasAddrSize || fil.Sectors.Length == 0 || (fil.Sectors.Length & 0xFF) != 0)
+                return false;
+
+            var metaOffset = (fil.Sectors.Length - 1) / 256 * 256;
+
+            if (fil.Sectors[metaOffset + 4] != 0xD6 || fil.Sectors[metaOffset + 5] != 0xD2)
+                return false;
+
+            result = new ImageMeta
+            {
+                DisplayMode = ParseDisplayMode(fil.Sectors, metaOffset),
+                IsGigaScreen = (fil.Sectors[metaOffset + 6] & 0x0F) == 0x0D,
+                PaletteType = ParsePaletteType(fil.Sectors, metaOffset),
+                CustomPalette = ParseCustomPalette(fil.Sectors, metaOffset),
+                Charset = ParseCharset(fil.Sectors, metaOffset),
+                Comment = ParseComment(fil.Sectors, metaOffset),
+            };
+
+            return true;
+        }
+
+        private static Mode ParseDisplayMode(byte[] data, int metaOffset)
+        {
+            byte code = data[metaOffset + 6];
+
+            // GigaScreen is Agat
+            if ((code & 0x0F) == 0x0D)
+                code &= 0xF0;
+
+            switch (code)
+            {
+                case 0x10: return Mode.Agat_256_256_Mono;
+                case 0x40: return Mode.Agat_64_64_Pal16;
+                case 0x50: return Mode.Agat_128_128_Pal16;
+                case 0x60: return Mode.Agat_256_256_Pal4;
+                case 0x70: return Mode.Agat_512_256_Mono;
+                case 0x80: return Mode.Agat_128_256_Pal16;
+                case 0x90: return Mode.Agat_280_192_AppleHiRes;
+                case 0x21: return Mode.Agat_T32_Pal16;
+                case 0x31: return Mode.Agat_T64_Mono;
+                case 0x41: return Mode.Agat_T64_Pal16;
+                case 0xA1: return Mode.Agat_T32_Pal16_FgBg;
+                case 0xC1: return Mode.Agat_T64_Pal16_FgBg;
+                case 0x9A: return Mode.Apple_280_192_HiRes;
+                case 0xAA: return Mode.Apple_T40;
+                case 0xBA: return Mode.Apple_T80;
+                case 0xCA: return Mode.Apple_40_48_LoRes;
+                case 0xDA: return Mode.Apple_80_48_DoubleLoRes;
+                case 0xEA: return Mode.Apple_140_192_DoubleHiResColor;
+                case 0xFA: return Mode.Apple_560_192_DoubleHiResMono;
+                default: return Mode.Unknown;
+            }
+        }
+
+        private static Palette ParsePaletteType(byte[] data, int metaOffset)
+        {
+            switch (data[metaOffset + 7] >> 4)
+            {
+                case 0: return Palette.Agat_1;
+                case 1: return Palette.Agat_2;
+                case 2: return Palette.Agat_3;
+                case 3: return Palette.Agat_4;
+                case 8: return Palette.Agat_1_Gray;
+                case 9: return Palette.Agat_2_Gray;
+                case 10: return Palette.Agat_3_Gray;
+                case 11: return Palette.Agat_4_Gray;
+                case 15: return Palette.Custom;
+                default: return Palette.Unknown;
+            }
+        }
+
+        private static IList<uint> ParseCustomPalette(byte[] data, int metaOffset)
+        {
+            var result = new uint[16];
+
+            for (var i = 0; i < 8; i++)
+            {
+                var or = metaOffset + 8 + i;
+                var og = metaOffset + 8 + 16 + i;
+                var ob = metaOffset + 8 + 32 + i;
+
+                result[i * 2] = Rgb444ToUint(data[or] >> 4, data[og] >> 4, data[ob] >> 4);
+                result[i * 2 + 1] = Rgb444ToUint(data[or] & 15, data[og] & 15, data[ob] & 15);
+            }
+
+            return result;
+        }
+
+        private static uint Rgb444ToUint(int r, int g, int b)
+        {
+            var ur = (uint)r;
+            var ug = (uint)g;
+            var ub = (uint)b;
+
+            return 0xFF000000 | (ur << 20) | (ur << 16) | (ug << 12) | (ug << 8) | (ub << 4) | ub;
+        }
+
+        private static string ParseCharset(byte[] data, int metaOffset)
+        {
+            return AgatEncoding.Decode(data.Skip(metaOffset + 16).Take(8).ToArray()).Trim();
+        }
+
+        private static string ParseComment(byte[] data, int metaOffset)
+        {
+            return AgatEncoding.Decode(data.Skip(metaOffset + 64).Take(192).ToArray()).Trim();
+        }
+    }
+}
