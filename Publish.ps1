cc1c5a4e1ac089e5071ee95bd39e0812ab082870
SnakE <snake.scaly@gmail.com>
2022-06-18 20:24:20 +0300

    Release publishing script

---
+++ Publish.ps1
@@ -0,0 +1,4 @@
+﻿$file = Get-ChildItem .\FilConvWpf\Properties\AssemblyInfo.cs
+$match = $file | Select-String -pattern "^\[assembly: AssemblyVersion\(`"(.*?)\.\d+`""
+$version = $match.Matches[0].Groups[1].Value
+dotnet publish FilConvWpf\FilConvWpf.csproj --output FilConv-$version --configuration Release -p:DebugType=None -p:DebugSymbols=false
