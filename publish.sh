de5b398bcb3a91ccd41da549ebaa4caa18b30488
SnakE <snake.scaly@gmail.com>
2024-05-01 20:49:40 +0300

    Updated publish script

--- publish.sh
+++ publish.sh
@@ -8,10 +8,17 @@ pub() {
     --configuration Release \
     -p:DebugType=None \
     -p:DebugSymbols=false \
+    -p:PublishSingleFile=true \
     "$@"
 }
 
 echo "AssemblyVersion $VERSION"
 
-pub --runtime linux-x64 --output FilConv-linux-$VERSION
-pub --runtime win-x64 --output FilConv-win-$VERSION
+pub --runtime linux-x64 --self-contained --output FilConv-linux-$VERSION
+pub --runtime linux-x64 --no-self-contained --output FilConv-linux-net8-$VERSION
+pub --runtime win-x64 --self-contained --output FilConv-win-$VERSION
+pub --runtime win-x64 --no-self-contained --output FilConv-win-net8-$VERSION
+
+for d in FilConv-*; do
+    7za a -tzip $d.zip $d
+done
