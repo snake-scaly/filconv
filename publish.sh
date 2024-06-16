a42f1758ce4b7482a722783d129a576db2dbcd1f
SnakE <snake.scaly@gmail.com>
2024-06-16 21:02:39 +0300

    Fix publish script to avoid zipping zips

--- publish.sh
+++ publish.sh
@@ -19,6 +19,6 @@ pub --runtime linux-x64 --no-self-contained --output FilConv-linux-net8-$VERSION
 pub --runtime win-x64 --self-contained --output FilConv-win-$VERSION
 pub --runtime win-x64 --no-self-contained --output FilConv-win-net8-$VERSION
 
-for d in FilConv-*; do
+for d in FilConv-*-$VERSION; do
     7za a -tzip $d.zip $d
 done
